9b248de45ff959d1508c84abfdc048665b983938
haiquangtran <hai.quang.tran@gmail.com>
2019-05-18 21:14:15 +1000

    added base repository class and IRepository interface.

---
+++ AE.CustomerApp.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using AE.CustomerApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AE.CustomerApp.Domain.Interfaces
+{
+    public interface IRepository<T> where T: class
+    {
+        IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate);
+        
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+        
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Save();
+    }
+}
---
+++ AE.CustomerApp.Infra.Data/Repository/BaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using AE.CustomerApp.Domain.Interfaces;
+using AE.CustomerApp.Domain.Models;
+using AE.CustomerApp.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AE.CustomerApp.Infra.Data.Repository
+{
+    public abstract class BaseRepository<T> : IRepository<T> where T : class
+    {
+        private readonly CustomerAppDbContext _customerAppDbContext;
+
+        public BaseRepository(CustomerAppDbContext customerAppDbContext)
+        {
+            _customerAppDbContext = customerAppDbContext;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindBy(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
