22fde86bad1d7dd5300292d1aa576b7603788ddf
haiquangtran <hai.quang.tran@gmail.com>
2019-05-18 20:45:49 +1000

    added Customer fields and baseEntity inside Domain project. Removed ValuesController

---
+++ AE.CustomerApp.Domain/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AE.CustomerApp.Domain.Models
+{
+    public abstract class BaseEntity
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public DateTime UpdatedDate { get; set; }
+    }
+}
--- AE.CustomerApp.Domain/Models/Customer.cs
+++ AE.CustomerApp.Domain/Models/Customer.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace AE.CustomerApp.Domain.Models
 {
-    public class Customer
+    [Table("Customer")]
+    public class Customer : BaseEntity
     {
+        [Required]
+        [StringLength(255)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
     }
 }
